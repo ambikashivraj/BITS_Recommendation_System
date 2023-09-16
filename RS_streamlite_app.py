fd0d20ac5e59c88e5983750dac02d3692b561ce1
ambikashivraj <144918116+ambikashivraj@users.noreply.github.com>
2023-09-16 12:47:48 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -46,7 +46,7 @@ selected_movie = st.selectbox(
     movie_list
 )
 
-if st.button('Show Recommendation'):
+if st.button('Show  Ambika's Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
